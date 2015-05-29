c75f59f59856d22c19853a0db2ad270f26374fab
mocxd <mark.adkins.hastings@gmail.com>
2015-05-29 02:30:16 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Raleway|Hind' rel='stylesheet' type='text/css'>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="b_main.css" rel="stylesheet" type="text/css" />
+	<link href="b_nav.css" rel="stylesheet" type="text/css" />
+	<link href="b_gal.css" rel="stylesheet" type="text/css" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="navpanel.js"></script>
+	<script type="text/javascript" src="gal.js"></script>
+	<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+	<meta name="viewport" content="width=device-width">
+	<title>Brian Wentz - Artist</title>
+</head>
+
+<body>
+
+<nav class="primary">
+
+<div class="clearfix">
+
+	<div class="titletxt">
+		<span><em>Brian Wentz</em> <span class="facont"><i class="fa fa-circle"></i></span> Artist</span>
+	</div>
+
+	<div class="navlinks clearfix">
+		<div class="valignhelper">
+			<ul class="toplinks clearfix">
+				<li class="topli" id="li1">
+					<a class="topa" href="#" id="cat1">Collage<span class="pointy" id="pointy1"></span></a>
+					<ul class="sublinks clearfix" id="sub1">
+						<li>
+							<a href="#">Gallery 1</a>
+						</li>
+						<li>
+							<a href="#">Gallery 2</a>
+						</li>
+					</ul>
+
+				</li>
+				<li class="topli" id="li2">
+					<a class="topa" href="#" id="cat2">Digital<span class="pointy" id="pointy2"></span></a>
+					<ul class="sublinks clearfix" id="sub2">
+						<li>
+							<a href="#">Animated Gifs</a>
+						</li>
+						<li>
+							<a href="#">Video</a>
+						</li>
+						<li>
+							<a href="#">Others</a>
+						</li>
+					</ul>
+
+				</li>
+				<li class="topli" id="li3">
+					<a class="topa" href="http://bawentz.storeenvy.com/" id="cat3">Store</a>
+				</li>
+				<li class="topli" id="li4">
+					<a class="topa" href="#" id="cat4">Contact<span class="pointy" id="pointy4"></span></a>
+					<ul class="sublinks clearfix" id="sub4">
+						<li>
+							<a href="mailto:bawentz@gmail.com"><i class="fa fa-envelope-o"></i> bawentz@gmail.com</a>
+						</li>
+					</ul>
+
+				</li>
+			</ul>
+		</div>
+	</div>
+
+</div>
+
+</nav>
+
+<div class="barcontainer">
+	<div class="colorbar" id="colorbar1">
+	&nbsp;
+	</div>
+	<div class="colorbar" id="colorbar2">
+	&nbsp;
+	</div>
+	<div class="colorbar" id="colorbar4">
+	&nbsp;
+	</div>
+</div>
+
+<main>
+
+<!-- php includes here -->
+
+	<section class="galleryitem">
+		<figure>
+			<img src="img/gal/012.jpg" />
+		</figure>
+		<figcaption>
+			<h1>012 Over Washington - Balloon Festival</h1>
+			<p>Original collage</p>
+			<p>12x12&quot; floating frame</p>
+			<p>2015</p>
+			<p class="price">$360</p>
+		</figcaption>
+	</section>
+
+	<section class="galleryitem">
+		<figure>
+			<img src="img/gal/017.jpg" />
+		</figure>
+		<figcaption>
+			<h1>017 Venus Ancestor</h1>
+			<p>Original collage on paper</p>
+			<p>7x7&quot; framed</p>
+			<p>2014</p>
+			<p class="price">$80</p>
+		</figcaption>
+	</section>
+
+	<section class="galleryitem">
+		<figure>
+			<img src="img/gal/023.jpg" />
+		</figure>
+		<figcaption>
+			<h1>023 Black Lips and Bruises</h1>
+			<p>Original collage on paper</p>
+			<p>6.6x8.5&quot; framed</p>
+			<p>2015</p>
+			<p class="price sold">Sold</p>
+		</figcaption>
+	</section>
+
+	<section class="galleryitem">
+		<figure>
+			<img src="img/gal/015.jpg" />
+		</figure>
+		<figcaption>
+			<h1>015 Red Gods</h1>
+			<p>Original collage on paper</p>
+			<p>10x12&quot; framed</p>
+			<p>2015</p>
+			<p class="price">$100</p>
+		</figcaption>
+	</section>
+
+	<nav class="pagination">
+		<ul class="pagelinks">
+			<li>
+			</li>
+		</ul>
+	</nav>
+
+</main>
+
+<footer>
+	<span class="foottxt">Site by <a href="http://handmadebits.com">handmadebits</a></span>
+</footer>
+
+</body>
+</html>
